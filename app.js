5e2d859dbbe52f41bb36b80456c27757a4f7f067
Dvontre Coleman <dvontrec@gmail.com>
2018-07-08 16:46:56 -0400

    Setup .env file

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const app = express();
 const PORT = process.env.PORT || 8008
+require('dotenv').config();
 
-app.get("/", (req, res) =>
-{
-	res.send({ by: 'buddy'});
-})
+// tells passport how to authenticate with the google oauth strategy
+// passport.use(new GoogleStrategy())
+
 
 //tells the app to listen on port 5000
 app.listen(PORT, ()=>
 {
-	console.log("app is running")
+	console.log("app is running", PORT);
 })
