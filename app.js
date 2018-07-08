f6862e0ad17ff011177b20bc25065e985f04f152
Dvontre Coleman <dvontrec@gmail.com>
2018-07-08 05:36:02 -0400

    Update for initial deployment

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8008
+
+app.get("/", (req, res) =>
+{
+	res.send({ hi: 'there'});
+})
+
+//tells the app to listen on port 5000
+app.listen(PORT, ()=>
+{
+	console.log("app is running")
+})
