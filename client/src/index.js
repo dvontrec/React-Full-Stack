2d04a59644204f698f41af5735c01391d852dec1
Dvontre Coleman <dvontrec@gmail.com>
2018-08-01 19:09:16 -0400

    Wire up redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App'
 
+const store = createStore(() => [], {}, applyMiddleware());
+
 //renders the app to the root div
-ReactDom.render(<App />, document.querySelector('#root'));
+ReactDom.render(
+	<Provider store={store}><App /></Provider>,
+	document.querySelector('#root')
+);
