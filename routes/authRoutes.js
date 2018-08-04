c0865a0a6c23d41e4f757431aeab3fb207752a53
Dvontre Coleman <dvontrec@gmail.com>
2018-08-03 21:07:06 -0400

    Setup login success redirection

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,28 +1,31 @@
-const express = require('express');
-const passport = require('passport');
+const express = require("express");
+const passport = require("passport");
 const router = express.Router();
 
 //authentication with google route
-router.get('/', 
-	passport.authenticate('google',		//tells the route to authenticate the user with google 
-	{
-		// asks google to give access to th eusers profile and emails
-		scope: ['profile', 'email']
-	})
+router.get(
+	"/",
+	passport.authenticate(
+		"google", //tells the route to authenticate the user with google
+		{
+			// asks google to give access to th eusers profile and emails
+			scope: ["profile", "email"]
+		}
+	)
 );
 
-// When user is logged in they 
-router.get('/callback', passport.authenticate('google'));
+// When user is logged in they
+router.get("/callback", passport.authenticate("google"), (req, res) => {
+	res.redirect("/surveys");
+});
 
-router.get('/api/logout', (req, res) =>
-{
+router.get("/api/logout", (req, res) => {
 	// Kills cookie saved in the sassion
 	req.logout();
 	res.send(req.user);
 });
 
-router.get('/api/current_user', (req, res) => 
-{
+router.get("/api/current_user", (req, res) => {
 	res.send(req.user);
 });
 
