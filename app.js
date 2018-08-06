b343fa3a908a2916e2625758f57ebfd13a324856
Dvontre Coleman <dvontrec@gmail.com>
2018-08-06 13:02:43 -0400

    Fix wildcard error

--- app.js
+++ app.js
@@ -39,8 +39,8 @@ if (process.env.NODE_ENV === 'production') {
 	// Express will serve production assets
 	app.use(express.static('client/build'));
 	// Express will serve index.html file for wildcard routes
-	const path = require('path')
-	app.get('*' (req, res) => {
+	const path = require('path');
+	app.get('*', (req, res) => {
 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
 }
